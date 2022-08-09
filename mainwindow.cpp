dc14e7be49700fb7b7e71cffc70d8f3b18c22962
Gambityasha <leopard69@rambler.ru>
2022-08-09 12:09:40 +0300

    Initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,72 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    logYear = QDate::currentDate().toString("yyyy");
+    logMonth = QDate::currentDate().toString("MM");
+    dirName = "./logs/"+logYear+"/";
+    dir.mkdir("./logs");
+    dir.mkdir("./logs/"+logYear);
+    setWindowIcon(QIcon("iconET.png"));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pbEnterTime_clicked()
+{
+
+    //QTime::currentTime().toString("HH:mm:ss")
+    QString h ;
+    if (ui->leHTime->text().toInt()<10) {
+        h="0"+ui->leHTime->text();
+    }else {
+        h=ui->leHTime->text();
+    }
+    QString m;
+            if (ui->leMTime->text().toInt()<10) {
+                m="0"+ui->leMTime->text();
+            }else {
+                m=ui->leMTime->text();
+            }
+
+
+    QString strTemp = h+":"+m+":00";
+    time = QTime::fromString(strTemp,"HH:mm:ss");
+    timeNew = time.addSecs(60*60*8+60*40);
+    ui->lblExitTime->setText(timeNew.toString("HH:mm:ss"));
+
+    fname=QDateTime::currentDateTime().toLocalTime().toString("MM")+"_ExitTime.txt";
+    file.setFileName("./logs/"+logYear+"/"+fname);
+    QTextStream stream(&file);
+    stream.setFieldAlignment(QTextStream::AlignLeft);
+    stream.setFieldWidth(32);
+    if (file.exists()){//Проверка - существует ли файл
+        if (file.open(QIODevice::WriteOnly | QIODevice::Append)) { // Append - для записи в конец файла
+            stream<<QDate::currentDate().toString("dd")+", "+QDate::currentDate().toString("dddd")<<" | " +time.toString("HH:mm:ss")<<" | " +timeNew.toString("HH:mm:ss");
+            stream.setFieldWidth(0);
+            stream<<"\r\n";
+            stream.setFieldWidth(32);
+        }
+    }else{
+        if (file.open(QIODevice::WriteOnly | QIODevice::Append)) { // Append - для записи в конец файла
+            stream<<QString::fromUtf8(" Дата")<<QString::fromUtf8(" | Время прихода")<<QString::fromUtf8(" | Расчетное время ухода");
+            stream.setFieldWidth(0);
+            stream<<"\r\n";
+            stream.setFieldWidth(32);
+            stream<<QDate::currentDate().toString("dd")+", "+QDate::currentDate().toString("dddd")<<" | "+time.toString("HH:mm:ss")<<" | " +timeNew.toString("HH:mm:ss");
+            stream.setFieldWidth(0);
+            stream<<"\r\n";
+            stream.setFieldWidth(32);
+        }
+    }
+    file.close();
+}
+
---
+++ mainwindow.h
@@ -0,0 +1,46 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFile>
+#include <QTextStream>
+#include <QDate>
+#include <QDateTime>
+#include <QTime>
+#include <QDir>
+#include <QMessageBox>
+#include <QPixmap>
+
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pbEnterTime_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QString fname;
+    QString logYear;
+    QString logMonth;
+    QString dirName;
+    QDir dir;
+    QTime time;
+    QTime timeNew;
+    QTime exitTime;
+    QFile file;
+
+
+
+};
+#endif // MAINWINDOW_H
